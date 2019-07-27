b3d12d5f827a6ca275f82add8c76e7ab6f55a94f
Giovanny <gio96gomez@gmail.com>
2019-07-27 11:25:34 -0500

    first commit dojo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Poke from './Pokedex/Poke'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div><Poke id="150" /></div>
   );
 }
-
 export default App;
